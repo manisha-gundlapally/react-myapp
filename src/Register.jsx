62ac1e1a13ecb440c4da36735834d03925c8be9b
Manisha Gundlapally <gundlapallymanisha4@gmail.com>
2026-03-11 11:48:07 +0530

    Update Register.jsx

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,11 +1,7 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Register() {
-
-  const navigate = useNavigate();
-
   const [data, setData] = useState({
     username: "",
     email: "",
@@ -13,37 +9,26 @@ function Register() {
   });
 
   const changeName = (e) => {
-    setData({
-      ...data,
-      [e.target.name]: e.target.value
-    });
+    setData({ ...data, [e.target.name]: e.target.value });
   };
 
   const submit = async () => {
     try {
-      const res = await axios.post(
-        "https://cabsystemsms-1.onrender.com/register",
-        data
-      );
-
-      console.log(res.data);
-
-      navigate("/h");
-
-    } catch (xyz) {
-      console.log(xyz);
+      const res = await axios.post("https://cabsystemsms-1.onrender.com/register", data);
+      alert(res.data);
+    } catch (err) {
+      const msg = err?.response?.data || err.message || 'Request failed';
+      alert(msg);
     }
   };
 
   return (
     <>
-      <h1>I am App</h1>
-
-      <input onChange={changeName} name="username" placeholder="enter name:" />
-      <input onChange={changeName} name="email" placeholder="enter email:" />
-      <input onChange={changeName} name="password" placeholder="enter password:" />
-
-      <button onClick={submit}>Submit</button>
+      <h1>Register</h1>
+      <input onChange={changeName} name="username" placeholder="enter Username" />
+      <input onChange={changeName} name="email" placeholder="enter email" />
+      <input type="password" onChange={changeName} name="password" placeholder="enter password" />
+      <button onClick={submit}>register</button>
     </>
   );
 }
